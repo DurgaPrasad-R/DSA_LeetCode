3f9af0632f67e524562250c6223b79886b1e2b4a
Runkana Durga Prasad <111352968+DurgaPrasad-R@users.noreply.github.com>
2024-02-03 22:04:39 +0530

    Time: 1 ms (100.00%) | Memory: 46.2 MB (15.13%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,25 +1,37 @@
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        ArrayList<Integer> finalArr = new ArrayList<>();
-        int n1 = nums1.length,n2 = nums2.length,new_len = n1+n2;
-        int i = 0,j = 0;
-        while(i<n1 && j<n2){
-            if (nums1[i] < nums2[j]){
-                finalArr.add(nums1[i++]);
-            } else {
-                finalArr.add(nums2[j++]);
+        int n1 = nums1.length,n2 = nums2.length;
+        if (n1 > n2){
+            return(isValidConfig(nums2,nums1,n2,n1));
+        } else {
+            return (isValidConfig(nums1,nums2,n1,n2));
+        }
+    }
+    public double isValidConfig(int[] arr1,int[] arr2, int n1,int n2){
+        int low = 0,high = n1;
+        int total = n1 + n2;
+        int leftEle = (total+1)/2;
+        while(low<=high){
+            int mid1 = (low+high)/2;
+            int mid2 = leftEle-mid1;
+            int l1 = Integer.MIN_VALUE,l2 = Integer.MIN_VALUE;
+            int r1 = Integer.MAX_VALUE,r2 = Integer.MAX_VALUE;
+            if (mid1 < n1) r1 = arr1[mid1];
+            if (mid2 < n2) r2 = arr2[mid2];
+            if (mid1 - 1 >= 0) l1 = arr1[mid1 - 1];
+            if (mid2 - 1 >= 0) l2 = arr2[mid2 - 1];
+            if (l1 <= r2 && l2 <= r1){
+                if (total%2 == 1){
+                    return Math.max(l1,l2);
+                } else {
+                    return (Math.max(l1,l2)+Math.min(r1,r2))/2.0;
+                }
+            }else if (l1 > r2){
+                high = mid1 - 1;
+            } else{
+                low = mid1 + 1;
             }
         }
-        while(i<n1){
-            finalArr.add(nums1[i++]);
-        }
-        while(j<n2){
-            finalArr.add(nums2[j++]);
-        }
-        if (new_len%2 != 0){
-            return (double)(finalArr.get(new_len/2));
-        } else {
-            return (double)((finalArr.get(new_len/2)+finalArr.get((new_len/2)-1))/2.0);
-        }
+        return -1/1.0;
     }
 }
