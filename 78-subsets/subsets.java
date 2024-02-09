0a18f79b0faedc6ee85163a9803c7bb7c6902a02
Runkana Durga Prasad <111352968+DurgaPrasad-R@users.noreply.github.com>
2024-02-09 19:45:44 +0530

    Time: 1 ms (69.52%) | Memory: 42.8 MB (33.30%) - LeetSync

---
+++ 78-subsets/subsets.java
@@ -0,0 +1,18 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] arr) {
+         List<List<Integer>> result = new ArrayList<>();
+        recursivePrint(0,arr,new ArrayList<>(), arr.length, result);
+        return(result);
+    }
+    public static void recursivePrint(int idx, int[] arr,List<Integer> ds, int len,List<List<Integer>> result){
+        if (idx == len){
+            result.add(new ArrayList<>(ds));
+            return;
+        } else {
+            ds.add(arr[idx]);
+            recursivePrint(idx+1,arr,ds,len,result);
+            ds.remove(ds.indexOf(arr[idx]));
+            recursivePrint(idx+1,arr,ds,len,result);
+        }
+    }
+}
