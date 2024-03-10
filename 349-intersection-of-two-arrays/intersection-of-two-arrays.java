9f51416d740f04c7072e57ab112a214674d2e49c
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-03-10 14:56:07 +0530

    Time: 0 ms (100.00%) | Memory: 42.4 MB (99.25%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.java
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.java
@@ -1,31 +1,23 @@
 class Solution {
     public int[] intersection(int[] nums1, int[] nums2) {
-        int n1 = nums1.length,n2 = nums2.length;
-        if (n1>n2){
-            return intersection(nums2,nums1);
-        } else {
-            Set<Integer> firstSet = new HashSet<>();
-            Set<Integer> commonSet = new HashSet<>();
-            List<Integer> resEle = new ArrayList<>();
-            for(int i = 0;i<n2;i++){
-                firstSet.add(nums2[i]);
+        int[] set = new int[1001];
+        for(int i:nums1) set[i] = 1;
+        int resCount = 0;
+        for(int i:nums2){
+            if (set[i] == 1){
+                set[i] = 2;
+                resCount+=1;
             }
-            for(int i = 0;i<n1;i++){
-                if (commonSet.contains(nums1[i])){
-                    continue;
-                } else {
-                    if (firstSet.contains(nums1[i])){
-                        commonSet.add(nums1[i]);
-                        resEle.add(nums1[i]);
-                    }
-                }
-            }
-            int resCnt = resEle.size();
-            int[] resArr = new int[resCnt];
-            for(int i = 0;i<resCnt;i++){
-                resArr[i] = resEle.get(i);
-            }
-            return resArr;
         }
+        int[] newArr = new int[resCount];
+        int[] nums = nums1.length>nums2.length?nums2:nums1;
+        int i = 0;
+        for(int v:nums){
+            if (set[v] == 2){
+                newArr[i++] = v;
+                set[v] = 1;
+            }
+        }
+        return newArr;
     }
 }
