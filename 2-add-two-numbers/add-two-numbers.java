712f57d188e6cca7009dfd76121f25d3e140c05b
Runkana Durga Prasad <runkanaprasad1@gmail.com>
2024-05-30 20:19:56 +0530

    Time: 1 ms (100.00%) | Memory: 44.3 MB (46.42%) - LeetSync

--- 2-add-two-numbers/add-two-numbers.java
+++ 2-add-two-numbers/add-two-numbers.java
@@ -10,29 +10,43 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode head1, ListNode head2) {
-        ListNode newHead = null;
+        ListNode newHead = head1;
         int sum = 0,carry = 0;
-        while(head1!=null || head2!=null || carry!=0){
-            sum = carry;
-            if (head1!=null){
-                sum+=head1.val;
-                head1 = head1.next;
-            }
-            if (head2!=null){
-                sum+=head2.val;
-                head2 = head2.next;
-            }
-            carry = sum/10;
-            sum = sum%10;
-            ListNode newNode = new ListNode(sum);
-            if (newHead == null){
-                newHead = newNode;
-            } else {
-                newNode.next = newHead;
-                newHead = newNode;
-            }
+        while (head1!=null && head2!=null) {
+            sum = head1.val + head2.val + carry;
+            carry = sum / 10;
+            head1.val = sum % 10;
+            head1 = head1.next;
+            head2 = head2.next;
         }
-        return reverseList(newHead);
+        while (head1 != null) {
+            sum = head1.val + carry;
+            carry = sum / 10;
+            head1.val = sum % 10;
+            head1 = head1.next;
+        }
+        ListNode tail = getTail(newHead);
+        while (head2 != null) {
+            sum = head2.val + carry;
+            carry = sum / 10;
+            ListNode newNode = new ListNode(sum % 10);
+            tail.next = newNode;
+            tail = newNode;
+            head2 = head2.next;
+        }
+        if (carry != 0) {
+            ListNode newNode = new ListNode(carry);
+            tail.next = newNode;
+            newNode = tail;
+        }
+        return newHead;
+    }
+    static ListNode getTail(ListNode head) {
+        ListNode temp = head;
+        while(temp.next!=null) {
+            temp = temp.next;
+        }
+        return temp;
     }
     static ListNode reverseList(ListNode head) {
         ListNode r = null, q = head.next, p = head;
